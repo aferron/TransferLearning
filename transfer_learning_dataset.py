debe15edbe1db0770c3eea8240ba8dffb5d5e213
Amila Ferron <aferron@pdx.edu>
2022-12-18 18:03:12 -0800

    Create custom dataset class

---
+++ transfer_learning_dataset.py
@@ -0,0 +1,14 @@
+from torch.utils.data import Dataset
+
+class TransferLearningDataset(Dataset):
+  def __init__(self, data, targets):
+    self.data = data
+    self.targets = targets
+
+  def __getitem__(self, index):
+    x = self.data[index]
+    y = self.targets[index]
+    return x, y
+
+  def __len__(self):
+    return len(self.data)
